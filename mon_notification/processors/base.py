2065cb1badd48eeef307b40b25ec4d38cd1cefc7
Tim Kuhlman <tim.kuhlman@hp.com>
2014-06-30 13:52:20 -0600

    Moved code out of __init__.py, minor pep8 fix

--- mon_notification/processors/base.py
+++ mon_notification/processors/base.py
@@ -13,4 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from base import BaseProcessor
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class BaseProcessor(object):
+    @staticmethod
+    def _add_to_queue(queue, queue_name, msg):
+        """Warns on full queue then does a blocking push to the queue.
+        """
+        if queue.full():
+            log.warn('Queue %s is full, publishing is blocked' % queue_name)
+        queue.put(msg)
+        log.debug("Put message %s on queue %s" % (msg, queue_name))
