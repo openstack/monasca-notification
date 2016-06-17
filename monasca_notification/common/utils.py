c74ea85c7121257d0bb880e2db7413b91465fc85
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-06-17 14:28:10 -0600

    Import simport from monasca-common
    Change-Id: Ibe6751b58717f1b72fe653983bf6088cecd9a158

--- monasca_notification/common/utils.py
+++ monasca_notification/common/utils.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import simport
+from monasca_common.simport import simport
 
 
 def get_db_repo(config):
