7faa2184cd0e2cd9c95785431984bf9cb1b6e987
Hervé Beraud <hberaud@redhat.com>
2020-06-09 01:25:09 +0200

    Use unittest.mock instead of mock
    The mock third party library was needed for mock support in py2
    runtimes. Since we now only support py36 and later, we can use the
    standard lib unittest.mock module instead.
    
    Change-Id: Ib7c9cca06dcfe14440ab9711cc0b76a5b4fe4e1c

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,7 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from mock import patch
+from unittest.mock import patch
 
 from monasca_notification.common import utils
 from tests import base
